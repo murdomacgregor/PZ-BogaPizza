7029ced7e142c869b10f4858cc0ce8610e8ab300
Murdo Macgregor <82396389+murdomacgregor@users.noreply.github.com>
2023-01-14 02:28:14 +0000

    Adding current V2 files
    Adding the files for V2 as of 14/01/23

---
+++ media/lua/server/items/BogaPizza-Distributions.lua
@@ -0,0 +1,362 @@
+require 'Items/SuburbsDistributions'
+require 'Items/ProceduralDistributions'
+
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, "BogaPizza.PizzaDough");
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, 10);
+
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, "BogaPizza.PizzaDough");
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, 10);
+
+
+
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, "BogaPizza.PizzaSauced");
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, 10);
+
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, "BogaPizza.PizzaSauced");
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, 10);
+
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, "BogaPizza.PizzaSaucedSlice");
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, 10);
+
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, "BogaPizza.PizzaSaucedSlice");
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, 10);
+
+
+
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, "BogaPizza.PizzaBBQSauced");
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, 10);
+
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, "BogaPizza.PizzaBBQSauced");
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, 10);
+
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, "BogaPizza.PizzaBBQSaucedSlice");
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, 10);
+
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, "BogaPizza.PizzaBBQSaucedSlice");
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, 10);
+
+
+
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, "BogaPizza.PizzaWhiteSauced");
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, 10);
+
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, "BogaPizza.PizzaWhiteSauced");
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, 10);
+
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, "BogaPizza.PizzaWhiteSaucedSlice");
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, 10);
+
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, "BogaPizza.PizzaWhiteSaucedSlice");
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, 10);
+
+
+
+table.insert(ProceduralDistributions.list["FridgeSnacks"].items, "BogaPizza.PizzaCheese");
+table.insert(ProceduralDistributions.list["FridgeSnacks"].items, 5);
+
+table.insert(ProceduralDistributions.list["MotelFridge"].items, "BogaPizza.PizzaCheese");
+table.insert(ProceduralDistributions.list["MotelFridge"].items, 3);
+
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, "BogaPizza.PizzaCheese");
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, 6);
+
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, "BogaPizza.PizzaCheese");
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, 7);
+
+table.insert(ProceduralDistributions.list["FridgeSnacks"].items, "BogaPizza.PizzaCheeseSlice");
+table.insert(ProceduralDistributions.list["FridgeSnacks"].items, 5);
+
+table.insert(ProceduralDistributions.list["MotelFridge"].items, "BogaPizza.PizzaCheeseSlice");
+table.insert(ProceduralDistributions.list["MotelFridge"].items, 3);
+
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, "BogaPizza.PizzaCheeseSlice");
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, 6);
+
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, "BogaPizza.PizzaCheeseSlice");
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, 7);
+
+
+
+
+table.insert(ProceduralDistributions.list["FridgeSnacks"].items, "BogaPizza.PizzaHamMushroom");
+table.insert(ProceduralDistributions.list["FridgeSnacks"].items, 5);
+
+table.insert(ProceduralDistributions.list["MotelFridge"].items, "BogaPizza.PizzaHamMushroom");
+table.insert(ProceduralDistributions.list["MotelFridge"].items, 3);
+
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, "BogaPizza.PizzaHamMushroom");
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, 6);
+
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, "BogaPizza.PizzaHamMushroom");
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, 7);
+
+table.insert(ProceduralDistributions.list["FridgeSnacks"].items, "BogaPizza.PizzaHamMushroomSlice");
+table.insert(ProceduralDistributions.list["FridgeSnacks"].items, 5);
+
+table.insert(ProceduralDistributions.list["MotelFridge"].items, "BogaPizza.PizzaHamMushroomSlice");
+table.insert(ProceduralDistributions.list["MotelFridge"].items, 3);
+
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, "BogaPizza.PizzaHamMushroomSlice");
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, 6);
+
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, "BogaPizza.PizzaHamMushroomSlice");
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, 7);
+
+
+table.insert(ProceduralDistributions.list["FridgeSnacks"].items, "BogaPizza.PizzaEtna");
+table.insert(ProceduralDistributions.list["FridgeSnacks"].items, 5);
+
+table.insert(ProceduralDistributions.list["MotelFridge"].items, "BogaPizza.PizzaEtna");
+table.insert(ProceduralDistributions.list["MotelFridge"].items, 3);
+
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, "BogaPizza.PizzaEtna");
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, 7);
+
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, "BogaPizza.PizzaEtna");
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, 6);
+
+
+table.insert(ProceduralDistributions.list["FridgeSnacks"].items, "BogaPizza.PizzaEtnaSlice");
+table.insert(ProceduralDistributions.list["FridgeSnacks"].items, 5);
+
+table.insert(ProceduralDistributions.list["MotelFridge"].items, "BogaPizza.PizzaEtnaSlice");
+table.insert(ProceduralDistributions.list["MotelFridge"].items, 3);
+
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, "BogaPizza.PizzaEtnaSlice");
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, 6);
+
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, "BogaPizza.PizzaEtnaSlice");
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, 7);
+
+
+table.insert(ProceduralDistributions.list["FridgeSnacks"].items, "BogaPizza.PizzaHawaii");
+table.insert(ProceduralDistributions.list["FridgeSnacks"].items, 10);
+
+table.insert(ProceduralDistributions.list["MotelFridge"].items, "BogaPizza.PizzaHawaii");
+table.insert(ProceduralDistributions.list["MotelFridge"].items, 10);
+
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, "BogaPizza.PizzaHawaii");
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, 8);
+
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, "BogaPizza.PizzaHawaii");
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, 7);
+
+table.insert(ProceduralDistributions.list["FridgeSnacks"].items, "BogaPizza.PizzaHawaiiSlice");
+table.insert(ProceduralDistributions.list["FridgeSnacks"].items, 5);
+
+table.insert(ProceduralDistributions.list["MotelFridge"].items, "BogaPizza.PizzaHawaiiSlice");
+table.insert(ProceduralDistributions.list["MotelFridge"].items, 3);
+
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, "BogaPizza.PizzaHawaiiSlice");
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, 6);
+
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, "BogaPizza.PizzaHawaiiSlice");
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, 7);
+
+
+
+table.insert(ProceduralDistributions.list["FridgeSnacks"].items, "BogaPizza.PizzaPineapple");
+table.insert(ProceduralDistributions.list["FridgeSnacks"].items, 10);
+
+table.insert(ProceduralDistributions.list["MotelFridge"].items, "BogaPizza.PizzaPineapple");
+table.insert(ProceduralDistributions.list["MotelFridge"].items, 10);
+
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, "BogaPizza.PizzaPineapple");
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, 8);
+
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, "BogaPizza.PizzaPineapple");
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, 7);
+
+table.insert(ProceduralDistributions.list["FridgeSnacks"].items, "BogaPizza.PizzaPineappleSlice");
+table.insert(ProceduralDistributions.list["FridgeSnacks"].items, 5);
+
+table.insert(ProceduralDistributions.list["MotelFridge"].items, "BogaPizza.PizzaPineappleSlice");
+table.insert(ProceduralDistributions.list["MotelFridge"].items, 3);
+
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, "BogaPizza.PizzaPineappleSlice");
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, 6);
+
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, "BogaPizza.PizzaPineappleSlice");
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, 7);
+
+table.insert(ProceduralDistributions.list["FridgeSnacks"].items, "BogaPizza.PizzaNduja");
+table.insert(ProceduralDistributions.list["FridgeSnacks"].items, 10);
+
+table.insert(ProceduralDistributions.list["MotelFridge"].items, "BogaPizza.PizzaNduja");
+table.insert(ProceduralDistributions.list["MotelFridge"].items, 10);
+
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, "BogaPizza.PizzaNduja");
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, 8);
+
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, "BogaPizza.PizzaNduja");
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, 7);
+
+table.insert(ProceduralDistributions.list["FridgeSnacks"].items, "BogaPizza.PizzaNdujaSlice");
+table.insert(ProceduralDistributions.list["FridgeSnacks"].items, 5);
+
+table.insert(ProceduralDistributions.list["MotelFridge"].items, "BogaPizza.PizzaNdujaSlice");
+table.insert(ProceduralDistributions.list["MotelFridge"].items, 3);
+
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, "BogaPizza.PizzaNdujaSlice");
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, 6);
+
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, "BogaPizza.PizzaNdujaSlice");
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, 7);
+
+
+
+table.insert(ProceduralDistributions.list["FridgeSnacks"].items, "BogaPizza.PizzaRumHam");
+table.insert(ProceduralDistributions.list["FridgeSnacks"].items, 2);
+
+table.insert(ProceduralDistributions.list["MotelFridge"].items, "BogaPizza.PizzaRumHam");
+table.insert(ProceduralDistributions.list["MotelFridge"].items, 1);
+
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, "BogaPizza.PizzaRumHam");
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, 1);
+
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, "BogaPizza.PizzaRumHam");
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, 2);
+
+
+table.insert(ProceduralDistributions.list["FridgeSnacks"].items, "BogaPizza.PizzaRumHamSlice");
+table.insert(ProceduralDistributions.list["FridgeSnacks"].items, 5);
+
+table.insert(ProceduralDistributions.list["MotelFridge"].items, "BogaPizza.PizzaRumHamSlice");
+table.insert(ProceduralDistributions.list["MotelFridge"].items, 3);
+
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, "BogaPizza.PizzaRumHamSlice");
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, 6);
+
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, "BogaPizza.PizzaRumHamSlice");
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, 7);
+
+
+table.insert(ProceduralDistributions.list["FridgeSnacks"].items, "BogaPizza.PizzaRumHamSlice");
+table.insert(ProceduralDistributions.list["FridgeSnacks"].items, 2);
+
+table.insert(ProceduralDistributions.list["MotelFridge"].items, "BogaPizza.PizzaRumHamSlice");
+table.insert(ProceduralDistributions.list["MotelFridge"].items, 1);
+
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, "BogaPizza.PizzaRumHamSlice");
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, 1);
+
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, "BogaPizza.PizzaRumHamSlice");
+table.insert(ProceduralDistributions.list["ServingTrayPizza"].items, 2);
+
+
+
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, "BogaPizza.DicedPineapple");
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, 20);
+
+
+
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, "BogaPizza.NdujaSausageSliced");
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, 20);
+
+table.insert(ProceduralDistributions.list["FridgeSnacks"].items, "BogaPizza.NdujaSausageSliced");
+table.insert(ProceduralDistributions.list["FridgeSnacks"].items, 7);
+
+
+
+table.insert(ProceduralDistributions.list["FridgeSnacks"].items, "BogaPizza.NdujaSausage");
+table.insert(ProceduralDistributions.list["FridgeSnacks"].items, 7);
+
+table.insert(ProceduralDistributions.list["MotelFridge"].items, "BogaPizza.NdujaSausage");
+table.insert(ProceduralDistributions.list["MotelFridge"].items, 6);
+
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, "BogaPizza.NdujaSausage");
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, 12);
+
+table.insert(ProceduralDistributions.list["ButcherSmoked"].items, "BogaPizza.NdujaSausage");
+table.insert(ProceduralDistributions.list["ButcherSmoked"].items, 8);
+
+table.insert(ProceduralDistributions.list["DinerKitchenFreezer"].items, "BogaPizza.NdujaSausage");
+table.insert(ProceduralDistributions.list["DinerKitchenFreezer"].items, 8);
+
+table.insert(ProceduralDistributions.list["DinerKitchenFridge"].items, "BogaPizza.NdujaSausage");
+table.insert(ProceduralDistributions.list["DinerKitchenFridge"].items, 8);
+
+table.insert(ProceduralDistributions.list["ButcherSmoked"].items, "BogaPizza.NdujaSausage");
+table.insert(ProceduralDistributions.list["ButcherSmoked"].items, 8);
+
+table.insert(ProceduralDistributions.list["Meat"].items, "BogaPizza.NdujaSausage");
+table.insert(ProceduralDistributions.list["Meat"].items, 8);
+
+
+
+table.insert(ProceduralDistributions.list["MotelFridge"].items, "BogaPizza.RumHam");
+table.insert(ProceduralDistributions.list["MotelFridge"].items, 3);
+
+table.insert(ProceduralDistributions.list["FridgeSnacks"].items, "BogaPizza.RumHam");
+table.insert(ProceduralDistributions.list["FridgeSnacks"].items, 4);
+
+table.insert(ProceduralDistributions.list["DinerKitchenFridge"].items, "BogaPizza.RumHam");
+table.insert(ProceduralDistributions.list["DinerKitchenFridge"].items, 4);
+
+table.insert(ProceduralDistributions.list["ButcherSmoked"].items, "BogaPizza.RumHam");
+table.insert(ProceduralDistributions.list["ButcherSmoked"].items, 5);
+
+table.insert(ProceduralDistributions.list["Meat"].items, "BogaPizza.RumHam");
+table.insert(ProceduralDistributions.list["Meat"].items, 6);
+
+
+
+table.insert(ProceduralDistributions.list["RestaurantKitchenFreezer"].items, "BogaPizza.NdujaSausage");
+table.insert(ProceduralDistributions.list["RestaurantKitchenFreezer"].items, 8);
+
+table.insert(ProceduralDistributions.list["RestaurantKitchenFridge"].items, "BogaPizza.NdujaSausage");
+table.insert(ProceduralDistributions.list["RestaurantKitchenFridge"].items, 8);
+
+
+table.insert(ProceduralDistributions.list["GroceryStandVegetables3"].items, "BogaPizza.ChiliPepper");
+table.insert(ProceduralDistributions.list["GroceryStandVegetables3"].items, 8);
+
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, "BogaPizza.ChiliPepper");
+table.insert(ProceduralDistributions.list["PizzaKitchenFridge"].items, 12);
+
+
+
+table.insert(ProceduralDistributions.list["BarCounterLiquor"].items, "BogaPizza.RumFull");
+table.insert(ProceduralDistributions.list["BarCounterLiquor"].items, 4);
+
+table.insert(ProceduralDistributions.list["BarCounterWeapon"].items, "BogaPizza.RumFull");
+table.insert(ProceduralDistributions.list["BarCounterWeapon"].items, 2);
+
+table.insert(ProceduralDistributions.list["BarShelfLiquor"].items, "BogaPizza.RumFull");
+table.insert(ProceduralDistributions.list["BarShelfLiquor"].items, 2);
+
+table.insert(ProceduralDistributions.list["KitchenBottles"].items, "BogaPizza.RumFull");
+table.insert(ProceduralDistributions.list["KitchenBottles"].items, 4);
+
+table.insert(ProceduralDistributions.list["MotelFridge"].items, "BogaPizza.RumFull");
+table.insert(ProceduralDistributions.list["MotelFridge"].items, 1);
+
+table.insert(ProceduralDistributions.list["StoreShelfWhiskey"].items, "BogaPizza.RumFull");
+table.insert(ProceduralDistributions.list["StoreShelfWhiskey"].items, 8);
+
+
+
+table.insert(ProceduralDistributions.list["GigamartSauce"].items, "BogaPizza.WhiteSauce");
+table.insert(ProceduralDistributions.list["GigamartSauce"].items, 10);
+
+table.insert(ProceduralDistributions.list["KitchenDryFood"].items, "BogaPizza.WhiteSauce");
+table.insert(ProceduralDistributions.list["KitchenDryFood"].items, 1);
+
+table.insert(ProceduralDistributions.list["PizzaKitchenSauce"].items, "BogaPizza.WhiteSauce");
+table.insert(ProceduralDistributions.list["PizzaKitchenSauce"].items, 10);
+
+table.insert(ProceduralDistributions.list["StoreKitchenSauce"].items, "BogaPizza.WhiteSauce");
+table.insert(ProceduralDistributions.list["StoreKitchenSauce"].items, 10);
+
+
+table.insert(ProceduralDistributions.list["GigamartSauce"].items, "BogaPizza.BBQSauce");
+table.insert(ProceduralDistributions.list["GigamartSauce"].items, 10);
+
+table.insert(ProceduralDistributions.list["KitchenDryFood"].items, "BogaPizza.BBQSauce");
+table.insert(ProceduralDistributions.list["KitchenDryFood"].items, 1);
+
+table.insert(ProceduralDistributions.list["PizzaKitchenSauce"].items, "BogaPizza.BBQSauce");
+table.insert(ProceduralDistributions.list["PizzaKitchenSauce"].items, 10);
+
+table.insert(ProceduralDistributions.list["StoreKitchenSauce"].items, "BogaPizza.BBQSauce");
+table.insert(ProceduralDistributions.list["StoreKitchenSauce"].items, 10);
